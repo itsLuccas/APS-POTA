ad7c55d1849821ca1d94f568d6ab933af6745078
Luccas de Felice Donaire <felice.donaire@gmail.com>
2020-10-14 12:06:23 -0300

    teste2

--- APS - POTA - Isabela Canelas Ett_21058253, Luccas de Felice Donaire_21202861/src/ordenar/Ordenar.java
+++ APS - POTA - Isabela Canelas Ett_21058253, Luccas de Felice Donaire_21202861/src/ordenar/Ordenar.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 // @author Isabela Ett_21058253 Luccas de Felice_21202861
 
-//teste porra
+//teste
 public class Ordenar {
 
     public static void randomize(int[] array) {
